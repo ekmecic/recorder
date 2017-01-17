7c898b3d112db2dd763e74485a730ee9fd748bbc
Emil Ekmečić <e.ekmecic@gmail.com>
2017-01-17 11:49:42 -0500

    Clean up the code and add channel selection
    This makes it possible to select how many channels will be sampled through a
    hardcoded variable in the source.
    The plan is to make it a command line flag soon.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,55 @@
 #include "../include/libpruio/src/pruio.h"
-#include "stdio.h"
+
 #include <stdint.h>
+#include <stdio.h>
 #include <unistd.h>
 
 int main() {
-  pruIo *io =
-      pruio_new(PRUIO_DEF_ACTIVE, 0x98, 0, 1); //! create new driver structure
+  const uint8_t numChannels = 2;
+  const uint32_t numSamples = 25000;
+  uint16_t dataArray[numChannels][numSamples];
 
+  pruIo *io = pruio_new(PRUIO_DEF_ACTIVE, 0, 0, 0);
   if (io->Errr) {
-    printf("initialisation failed (%s)\n", io->Errr);
+    printf("Initialization failed (%s)\n", io->Errr);
   }
 
   if (pruio_config(io, 1, 0x1FE, 0, 4)) {
-    printf("config failed (%s)\n", io->Errr);
+    printf("Configuration failed failed (%s)\n", io->Errr);
   }
 
-  FILE *datafile = fopen("output.dat", "w+");
-  uint32_t i = 0;
-  while (i < 2000) {
-    fprintf(datafile, "%d,%d,%d,%d\n", i,
-            io->Adc->Value[0],
-            io->Adc->Value[1],
-            io->Adc->Value[2]
-            );
-    i += 1;
-    usleep(500);
+  // Configure 6 channels of data measurement, AIN-1 thru AIN-6
+  for (int i = 0; i < (numChannels - 1); i++) {
+    if (pruio_adc_setStep(io, (i + 10), i, 0, 0, 0)) {
+      printf("AIN-%d configuration failed with error message %s", i, io->Errr);
+    }
+  }
+
+  struct timeval  tv1, tv2;
+  gettimeofday(&tv1, NULL);
+  // Data capture routine
+  for (int i = 0; i < numSamples; i++) {
+    for (int j = 0; j < numChannels; j++) {
+      dataArray[j][i] = io->Adc->Value[j + 1];
+    }
+    usleep(10);
+  }
+  gettimeofday(&tv2, NULL);
+
+  printf ("Total time to capture %d samples was %f seconds\n",
+          numSamples,
+          (double) (tv2.tv_usec - tv1.tv_usec) / 1000000 +
+          (double) (tv2.tv_sec - tv1.tv_sec));
+
+  FILE *datafile = fopen("output.txt", "w+");
+  // Data dumping routine
+  for (int i = 0; i < numSamples; i++) {
+    fprintf(datafile, "%d,", i);
+    for (int j = 0; j < (numChannels - 1); j++) {
+      fprintf(datafile, "%d,", dataArray[j][i]);
+    }
+    // We do the last one separately to avoid a trailing comma/add a newline
+    fprintf(datafile, "%d\n", dataArray[numChannels - 1][i]);
   }
 
   fclose(datafile);
